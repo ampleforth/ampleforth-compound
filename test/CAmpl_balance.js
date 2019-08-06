abaf318d8b1ce905a3407e048892a989fcfafb59
nithin <nithin@fragments.org>
2019-08-06 15:06:14 -0700

    moved initial exchange rate out as a constnat

--- test/CAmpl_balance.js
+++ test/CAmpl_balance.js
@@ -18,9 +18,11 @@ function toAmplDecimals(x){
   return (new BN(x)).mul(new BN(10 ** AMPL_DECIMALS));
 }
 
-const INITIAL_SUPPLY = toAmplDecimals(50000000);
-const MINT_SUPPLY = toAmplDecimals(20000000);
-const SUPPLY_DELTA_10 = toAmplDecimals(5000000); // 10%
+const INITIAL_AMPL_SUPPLY = toAmplDecimals(50000000);
+const SUPPLY_DELTA_10 = INITIAL_AMPL_SUPPLY.div(new BN(10)); // 10%
+
+const INITIAL_EXCHANGE_RATE = new BN(1);
+const MINT_cAMPL_SUPPLY = toAmplDecimals(20000000).mul(INITIAL_EXCHANGE_RATE);
 
 let ampl, cAmpl, owner, anotherAccount, recipient, r;
 async function setupContractAndAccounts (accounts) {
@@ -41,17 +43,13 @@ async function setupContractAndAccounts (accounts) {
 
   await comptroller._supportMarket(cAmpl.address);
   await comptroller._supportMarket(cEther.address);
-  await ampl.approve(cAmpl.address, INITIAL_SUPPLY);
 }
 
 contract('CAmpl', function (accounts) {
   beforeEach('setup CAmpl contract', async function () {
     await setupContractAndAccounts(accounts);
-    await cAmpl.mint(MINT_SUPPLY, {from:owner});
-    expect(await cAmpl.totalSupply.call()).to.eq.BN(MINT_SUPPLY);
-    expect(await cAmpl.balanceOf.call(owner)).to.eq.BN(MINT_SUPPLY);
-    expect(await cAmpl.balanceOfUnderlying.call(owner)).to.eq.BN(MINT_SUPPLY);
-    expect(await ampl.balanceOf.call(cAmpl.address)).to.eq.BN(MINT_SUPPLY);
+    await ampl.approve(cAmpl.address, MINT_cAMPL_SUPPLY);
+    await cAmpl.mint(MINT_cAMPL_SUPPLY, {from:owner});
   });
 
   describe('when rebase increases AMPL supply', function () {
@@ -59,10 +57,10 @@ contract('CAmpl', function (accounts) {
       await ampl.rebase(1, SUPPLY_DELTA_10); // +10% supply
     });
     it('should not update the totalSupply', async function() {
-      expect(await cAmpl.totalSupply.call()).to.eq.BN(MINT_SUPPLY);
+      expect(await cAmpl.totalSupply.call()).to.eq.BN(MINT_cAMPL_SUPPLY);
     });
     it('should not update the cAmpl balance', async function () {
-      expect(await cAmpl.balanceOf.call(owner)).to.eq.BN(MINT_SUPPLY);
+      expect(await cAmpl.balanceOf.call(owner)).to.eq.BN(MINT_cAMPL_SUPPLY);
     });
     it('should update the underlying ampl balance', async function () {
       expect(await cAmpl.balanceOfUnderlying.call(owner)).to.eq.BN(toAmplDecimals(22000000));
@@ -75,10 +73,10 @@ contract('CAmpl', function (accounts) {
       await ampl.rebase(1, -SUPPLY_DELTA_10); // -10% supply
     });
     it('should not update the totalSupply', async function() {
-      expect(await cAmpl.totalSupply.call()).to.eq.BN(MINT_SUPPLY);
+      expect(await cAmpl.totalSupply.call()).to.eq.BN(MINT_cAMPL_SUPPLY);
     });
     it('should not update the cAmpl balance', async function () {
-      expect(await cAmpl.balanceOf.call(owner)).to.eq.BN(MINT_SUPPLY);
+      expect(await cAmpl.balanceOf.call(owner)).to.eq.BN(MINT_cAMPL_SUPPLY);
     });
     it('should update the underlying ampl balance', async function () {
       expect(await cAmpl.balanceOfUnderlying.call(owner)).to.eq.BN(toAmplDecimals(18000000));
@@ -91,14 +89,14 @@ contract('CAmpl', function (accounts) {
       await ampl.rebase(1, 0);
     });
     it('should not update the totalSupply', async function() {
-      expect(await cAmpl.totalSupply.call()).to.eq.BN(MINT_SUPPLY);
+      expect(await cAmpl.totalSupply.call()).to.eq.BN(MINT_cAMPL_SUPPLY);
     });
     it('should not update the cAmpl balance', async function () {
-      expect(await cAmpl.balanceOf.call(owner)).to.eq.BN(MINT_SUPPLY);
+      expect(await cAmpl.balanceOf.call(owner)).to.eq.BN(MINT_cAMPL_SUPPLY);
     });
     it('should update the underlying ampl balance', async function () {
-      expect(await cAmpl.balanceOfUnderlying.call(owner)).to.eq.BN(MINT_SUPPLY);
-      expect(await ampl.balanceOf.call(cAmpl.address)).to.eq.BN(MINT_SUPPLY);
+      expect(await cAmpl.balanceOfUnderlying.call(owner)).to.eq.BN(MINT_cAMPL_SUPPLY);
+      expect(await ampl.balanceOf.call(cAmpl.address)).to.eq.BN(MINT_cAMPL_SUPPLY);
     });
   });
 });
