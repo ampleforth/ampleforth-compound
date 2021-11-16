3ad613cc3d216afc707d8ad5b5b2b765c2a9ba70
nithin <nithin@fragments.org>
2021-11-16 13:15:43 -0500

    intermediate

---
+++ contracts/CErc20.sol
@@ -0,0 +1,3 @@
+pragma solidity ^0.5.8;
+
+import {CErc20} from "compound-protocol/contracts/CErc20.sol";
---
+++ hardhat.config.ts
@@ -0,0 +1,33 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.5.16'
+      }
+    ]
+  },
+  mocha: {
+    timeout: 1000000
+  },
+  networks: { },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
